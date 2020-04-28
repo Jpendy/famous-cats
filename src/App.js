2aac2c26f3e2f2bd838297a39a6aa302240ceb21
Jpendy <Jpendy256@gmail.com>
2020-04-28 16:01:00 -0700

    got the app working.. forgot to commit earlier

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import images from './data'
+import Animal from './Animal'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+    state = { selected: null };
+
+    handleChange = (e) => {
+      this.setState({ selected: e.target.value });
+    };
+
+  render() {
+    return (
+      <div>         
+         <main>
+                    <section className = "dropown-section">
+                         <select className="dropdown-filter" onChange={this.handleChange}>
+                             <option value="">All Types</option>
+                             <option value="narwhal"> narwhal </option>
+                             <option value= "rhino"> rhino </option>
+                             <option value="triceratops"> triceratops </option>
+                             <option value ="lizard"> lizard </option>
+                             <option value="markhor"> markhor </option>
+                             <option value="chameleon"> chameleon </option>
+                             <option value="dragon"> dragon </option>
+                             <option value="mouflon"> mouflon </option>
+                             <option value= "addax"> addax </option>
+                             <option value="unicorn"> unicorn </option>
+                             <option value= "unilego"> unilego </option>
+                                   
+                         </select>
+                    </section>
+
+                    <section className="list-section">
+                      <ul className="animals">
+                        {
+                          images
+                            .filter(animal => {
+                              if(!this.state.selected) return true;
+
+                              return animal.keyword === this.state.selected;
+                            })
+                            .map( hornAnimal => {
+                              return <Animal hornType = { hornAnimal } />
+                            })
+
+                        }
+                      </ul>
+                    </section>
+                </main>               
+      </div>
+    )
+  }
 }
-
-export default App;
